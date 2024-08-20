a5fabb7f99515811c41d7f825dbc4ab09d3b0c74
PeggyYuchunWang <peggy.yuchun.wang@gmail.com>
2024-08-20 16:21:55 -0700

    add analytics api

--- src/utils/analytics.py
+++ src/utils/analytics.py
@@ -26,19 +26,43 @@ def log_async(category: str, content: str):
     thread.start()
 
 
-def log_sync_audio(filepath: str, file_base_path: str, player_id: str):
+def log_sync_audio(filepath: str, file_base_path: str, player_id: str, timestamp: str):
     try:
         with open(filepath, 'rb') as f:
             supabase.storage.from_("audio").upload(
-                file=f, path="audio/" + player_id + f"/{file_base_path}", file_options={"content-type": "audio/wav"})
+                file=f,
+                path="audio/" + player_id + f"/{timestamp}" + f"/{file_base_path}",
+                file_options={"content-type": "audio/wav"},
+            )
 
     except Exception as e:
         print(f"Failed to log audio to Supabase: {e}")
 
 
-def log_async_audio(filepath: str, file_base_path: str, player_id: str):
-    thread = threading.Thread(target=log_sync_audio, args=(
-        filepath, file_base_path, player_id))
+def log_async_audio(filepath: str, file_base_path: str, player_id: str, timestamp: str):
+    thread = threading.Thread(
+        target=log_sync_audio, args=(filepath, file_base_path, player_id, timestamp)
+    )
+    thread.start()
+
+
+def log_sync_game_logs(filepath: str, file_base_path: str, player_id: str):
+    try:
+        with open(filepath, "rb") as f:
+            supabase.storage.from_("textlogs").upload(
+                file=f,
+                path="textlogs/" + player_id + f"/{file_base_path}",
+                file_options={"content-type": "audio/wav"},
+            )
+
+    except Exception as e:
+        print(f"Failed to log game logs to Supabase: {e}")
+
+
+def log_async_game_logs(filepath: str, file_base_path: str, player_id: str):
+    thread = threading.Thread(
+        target=log_sync_game_logs, args=(filepath, file_base_path, player_id)
+    )
     thread.start()
 
 
