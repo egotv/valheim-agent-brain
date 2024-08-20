160ac66df7b10252696938e18889a2f33e1d4d75
PeggyYuchunWang <peggy.yuchun.wang@gmail.com>
2024-08-20 16:23:02 -0700

    delete unused code

--- src/utils/analytics.py
+++ src/utils/analytics.py
@@ -46,26 +46,6 @@ def log_async_audio(filepath: str, file_base_path: str, player_id: str, timestam
     thread.start()
 
 
-def log_sync_game_logs(filepath: str, file_base_path: str, player_id: str):
-    try:
-        with open(filepath, "rb") as f:
-            supabase.storage.from_("textlogs").upload(
-                file=f,
-                path="textlogs/" + player_id + f"/{file_base_path}",
-                file_options={"content-type": "audio/wav"},
-            )
-
-    except Exception as e:
-        print(f"Failed to log game logs to Supabase: {e}")
-
-
-def log_async_game_logs(filepath: str, file_base_path: str, player_id: str):
-    thread = threading.Thread(
-        target=log_sync_game_logs, args=(filepath, file_base_path, player_id)
-    )
-    thread.start()
-
-
 # Example usage
 # if __name__ == "__main__":
 #     log_async_audio("./audio_files/Recording.wav",
